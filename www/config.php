8d371c67527e4831163522c07bf2f1e773ecd769
Niclas Björner <niclas@cromigon.se>
2014-03-16 18:06:25 +0000

    Cleaned up the config.php file
    This was done to make things more generic. The necessary changes
    will be noted in the README. The comment regarding hidden.php
    was removed since the example now exists as a file.

--- www/config.php
+++ www/config.php
@@ -4,10 +4,10 @@ ini_set("display_errors", "1");
 error_reporting(-1);
 require_once dirname(__DIR__)."/hidden.php";
 
-$icecastHost = "cromigon.se";
+$icecastHost = "YOUR_HOST_HERE";
 $icecastPort = 8000;
 $icecastMount = "/stream";
-$radioTitle = "Music Radio";
+$radioTitle = "YOUR_TITLE_HERE";
 
 $trackWait = 4*60; // In minutes
 $queueWait = 20; // In minutes
@@ -16,22 +16,6 @@ $suggestWait = 12*60;
 
 $searchResultsPerPage = 20;
 $suggesionsPerPage = 20;
-
-// Make a hidden.php in the parent folder that looks like this:
-/*
-	<?php
-	$mysqlHost = 'localhost';
-	$mysqlUser = 'USERNAME';
-	$mysqlPassword = 'PASSWORD';
-	$mysqlDatabase = 'DATABASE';
-	$mpdPassword = $mysqlPassword;
-	$adminPass = $mysqlPassword;
-
-	$localIcecastHost = '127.0.0.1';
-	$localIcecastPort = 8000;
-
-	$musicPath = "/path/to/mpd's/music/folder/";
-*/
 
 $streamUrl = "http://".$icecastHost.":".$icecastPort.$icecastMount;
 $icecastpInfoUrl = "http://".$localIcecastHost.":".$localIcecastPort."/info_json.xsl";
